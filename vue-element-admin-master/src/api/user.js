727eb4cbe7236df8808d3196caf34f93824b737d
yy <yy@123.com>
2026-04-17 15:24:20 +0800

    delete unuseful

--- vue-element-admin-master/src/api/user.js
+++ vue-element-admin-master/src/api/user.js
@@ -1,24 +1,22 @@
 import request from '@/utils/request'
 
-export function login(data) {
-  return request({
-    url: '/vue-element-admin/user/login',
-    method: 'post',
-    data
-  })
+
+import requestHttp from '@/utils/request'
+//登录
+export async function login(data) {
+  return await requestHttp.login('/rental/user/login', data)
+}
+//获取用户信息
+export async function getInfo() {
+  return await requestHttp.get('/rental/auth/getInfo')
 }
 
-export function getInfo(token) {
-  return request({
-    url: '/vue-element-admin/user/info',
-    method: 'get',
-    params: { token }
-  })
-}
 
-export function logout() {
-  return request({
-    url: '/vue-element-admin/user/logout',
-    method: 'post'
-  })
-}
+
+
+// export function logout() {
+//   return request({
+//     url: '/vue-element-admin/user/logout',
+//     method: 'post'
+//   })
+// }
--- vue-element-admin-master/src/utils/request.js
+++ vue-element-admin-master/src/utils/request.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
+import qs from 'qs'
+
 
 // create an axios instance
 const service = axios.create({
@@ -19,7 +21,7 @@ service.interceptors.request.use(
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      config.headers['token'] = getToken()
     }
     return config
   },
@@ -46,7 +48,7 @@ service.interceptors.response.use(
     const res = response.data
 
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
+    if (res.code !== 200) {
       Message({
         message: res.message || 'Error',
         type: 'error',
@@ -56,9 +58,9 @@ service.interceptors.response.use(
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
         // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
+        MessageBox.confirm('您已注销,您可以取消以留在此页面,也可以重新登录', '确认退出', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
           store.dispatch('user/resetToken').then(() => {
@@ -82,4 +84,105 @@ service.interceptors.response.use(
   }
 )
 
-export default service
+const requestHttp = {
+  post(url, params) {
+    return service.post(url, params, {
+      transformRequest: [(params) => {
+        return JSON.stringify(params)
+      }],
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  },
+  put(url, params) {
+    return service.put(url, params, {
+      transformRequest: [(params) => {
+        return JSON.stringify(params)
+      }],
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  },
+  get(url, params) {
+    return service.get(url, {
+      params: params,
+      paramsSerializer: (params) => {
+        return qs.stringify(params)
+      }
+    })
+  },
+  getRestFulApi(url, params) {
+    let _params
+    if (Object.is(params, undefined || null)) {
+      _params = ''
+    } else {
+      _params = '/'
+      for (const key in params) {
+        if (params.hasOwnProperty(key) && params[key] !== null &&
+          params[key] !== '') {
+          _params += `${params[key]}/`
+        }
+      }
+      _params = _params.substr(0, _params.length - 1)
+      if (_params) {
+        return service.get(`${url}${_params}`)
+      } else {
+        return service.get(url)
+      }
+    }
+  },
+  delete(url, params) {
+    let _params
+    if (Object.is(params, undefined || null)) {
+      _params = ''
+    } else {
+      _params = '/'
+      for (const key in params) {
+        if (params.hasOwnProperty(key) && params[key] !== null &&
+          params[key] !== '') {
+          _params += `${params[key]}/`
+        }
+      }
+      _params = _params.substr(0, _params.length - 1)
+    }
+    if (_params) {
+      return service.delete(`${url}${_params}`).catch(err => {
+        message.error(err.msg)
+        return Promise.reject(err)
+      })
+    } else {
+      return service.delete(url).catch(err => {
+        message.error(err.msg)
+        return Promise.reject(err)
+      })
+    }
+  },
+  upload(url, params) {
+    return service.post(url, params, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+  },
+  login(url, params) {
+    return service.post(url, params, {
+      transformRequest: [(params) => {
+        return qs.stringify(params)
+      }],
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+  }
+}
+export default requestHttp
+
+
+// export default service
+
+
+
+
+
